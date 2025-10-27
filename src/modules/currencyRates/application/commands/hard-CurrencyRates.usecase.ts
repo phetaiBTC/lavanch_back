4639312d8783f786f5f866687f792ab7a37c8fd1
phetaibtc <phetaibtc@gmail.com>
2025-10-27 15:29:05 +0700

    update

--- src/modules/currencyRates/application/commands/hard-CurrencyRates.usecase.ts
+++ src/modules/currencyRates/application/commands/hard-CurrencyRates.usecase.ts
@@ -3,15 +3,16 @@ import {
   CURRENCYRATES_REPOSITORY,
   type ICurrencyRatesRepository,
 } from '../../domain/currencyrates.repository';
+import { FindOneCurrencyRatesUseCase } from '../queries/findOne-CurrencyRates.usecase';
 @Injectable()
 export class HardDeleteCurrencyRatesUseCase {
   constructor(
     @Inject(CURRENCYRATES_REPOSITORY)
     private readonly currencyratesRepo: ICurrencyRatesRepository,
+    private readonly findOneCurrencyRatesUseCase: FindOneCurrencyRatesUseCase,
   ) {}
   async execute(id: number): Promise<{ message: string }> {
-    const currencyrates = await this.currencyratesRepo.findById(id);
-    if (!currencyrates) throw new NotFoundException('CurrencyRates not found');
+    await this.findOneCurrencyRatesUseCase.execute(id);
     return this.currencyratesRepo.hardDelete(id);
   }
 }
